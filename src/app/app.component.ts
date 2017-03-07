3a3758c170996d3095942049669b8dfc76435d4c
Pieter Beekman <pieter.beekman@incentro.com>
2017-03-07 18:27:18 +0100

    Start opdracht 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   supermarketName: string;
+  products: any;
 
   constructor(){
     this.supermarketName = 'Jumbo';
+
+    this.products = [
+        {name: 'Sinaasappels', quantity: 25},
+        {name: 'Appels', quantity: 50},
+        {name: 'Citroenen', quantity: 5},
+        {name: 'Mandarijnen', quantity: 15},
+        {name: 'Bananen', quantity: 31}
+    ]
   }
 }
